4f41d35049af7254c0b83bc0d420e159b09f7542
ritikchhimwal <ritikchhimwal822@gmail.com>
2024-12-30 13:13:08 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,27 @@
 
 import './../styles/App.css';
 import React from 'react';
-import { Route, Routes, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
 const App = () => {
   return (
-    <div>
+    <Router>
       <nav>
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
         </ul>
       </nav>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </div>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </Router>
   );
 };
+
 
 export default App;
