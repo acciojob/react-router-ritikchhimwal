6dd457c4451029238af68a4745961e0060cd6258
Ritik Chhimwal <132690630+ritikchhimwal@users.noreply.github.com>
2024-12-30 13:01:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@
 import './../styles/App.css';
 import React from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
+import Home from './Home';
+import About from './About';
 
 const App = () => {
   return (
