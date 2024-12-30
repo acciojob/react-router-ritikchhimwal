a9a91ff984c35b7c856e437a68ea97fdd011b296
Ritik Chhimwal <132690630+ritikchhimwal@users.noreply.github.com>
2024-12-30 13:00:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
 
-import React from "react";
 import './../styles/App.css';
+import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </nav>
 
-export default App
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
