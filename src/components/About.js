58d4f9667bdbb801ae81a6fc3950faade518f339
Ritik Chhimwal <132690630+ritikchhimwal@users.noreply.github.com>
2024-12-30 13:01:21 +0530

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About This Application</h1>
+      <p>This application demonstrates basic React Router capabilities.</p>
+    </div>
+  );
+};
+
+export default About;
